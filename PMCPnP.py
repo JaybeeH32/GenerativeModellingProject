8416e9c1149e00b41fa38e1e3774b70e9902691e
BenjaminisCoding <benjamin.jean.lapostolle@gmail.com>
2024-03-22 20:52:20 +0100

    Implemented weights annealing, and did some experiments

--- PMCPnP.py
+++ PMCPnP.py
@@ -52,28 +52,38 @@ class PMCPnP(dinv.sampling.MonteCarlo):
         
         
 class PMCPnPAnnealingIterator(torch.nn.Module):
-    def __init__(self, gamma, sigmas, alphas=torch.ones(1000), max_iter=1e3):
+    def __init__(self, gamma, sigmas, alphas=torch.ones(1000), max_iter=1e7, deterministic = False, Himbert = False):
         super().__init__()
         self.gamma = gamma
         self.alphas = alphas
         self.noise_std = np.sqrt(2 * gamma)
         self.sigmas = sigmas
-        self.iter = 0
+        self.iter = -1
         self.max_iter = max_iter
+        self.deterministic = deterministic
+        self.Himbert = Himbert
 
     def forward(self, x, y, physics, likelihood, prior):
-        noise = torch.randn_like(x) * self.noise_std
-        lhood = - likelihood.grad(x, y, physics)
-        lprior = - prior(x - (self.gamma * lhood), self.sigmas[self.iter]) * self.alphas[self.iter]
-        self.iter = (self.iter + 1) % self.max_iter
-        return x + self.gamma * (lhood + lprior) + noise
-        
+        self.iter += 1
+        if not self.Himbert:
+            lhood = - likelihood.grad(x, y, physics)
+            lprior = - prior(x + (self.gamma * lhood), self.sigmas[self.iter]) * self.alphas[self.iter]
+
+        else:
+            lhood = - likelihood.grad(x, y, physics)
+            lprior = - prior(x - (self.gamma * lhood), self.sigmas[self.iter]) * self.alphas[self.iter]
+        if self.deterministic:
+            return x + self.gamma * (lhood + lprior) 
+        else:
+            noise = torch.randn_like(x) * self.noise_std
+            return x + self.gamma * (lhood + lprior) + noise 
+
 class PMCPnPAnnealing(dinv.sampling.MonteCarlo):
     def __init__(
-        self, prior, data_fidelity, sigmas, gamma, alphas, max_iter=1e3, thinning=1, burnin_ratio=0.4, clip=(-1, 2), verbose=True,
+        self, prior, data_fidelity, sigmas, gamma, alphas, max_iter=1e3, thinning=1, burnin_ratio=0.4, clip=(-1, 2), verbose=True, deterministic = False, Himbert = False,
     ):
         # generate an iterator
-        iterator = PMCPnPAnnealingIterator(gamma=gamma, sigmas=sigmas, alphas=alphas, max_iter=max_iter)
+        iterator = PMCPnPAnnealingIterator(gamma=gamma, sigmas=sigmas, alphas=alphas, max_iter=max_iter,  deterministic = deterministic, Himbert = Himbert)
         # set the params of the base class
         super().__init__(
             iterator, prior, data_fidelity, max_iter=max_iter, thinning=thinning, burnin_ratio=burnin_ratio, clip=clip, verbose=verbose,
